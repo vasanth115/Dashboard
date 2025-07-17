c531458876df72acf65600246f7c125d498c8c9d
Vasanth K <vasanth@galenhealth.ai>
2025-07-17 10:52:07 +0530

    Leaf

--- src/Components/HeaderElement.tsx
+++ src/Components/HeaderElement.tsx
@@ -1,26 +1,29 @@
-import { Flex, Typography , Avatar } from 'antd'
-import Search from "antd/es/input/Search"
-import { MessageOutlined , NotificationOutlined , UserOutlined } from '@ant-design/icons'
-
+import { Flex, Typography, Avatar } from "antd";
+import Search from "antd/es/input/Search";
+import {
+  MessageOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 const HeaderElement = () => {
   return (
-    <Flex align='center' justify='space-between'>
+    <Flex align="center" justify="space-between">
       <Typography.Title level={3} type="secondary">
-        Welcome Back , Vasanth 
+        Welcome Back To Leaf Community
       </Typography.Title>
 
-      <Flex align="center" gap='3rem'>
-        <Search placeholder="Search Here..." allowClear/>
+      <Flex align="center" gap="3rem">
+        <Search placeholder="Search Here..." allowClear />
 
-        <Flex align="center" gap='10px'>
-            <MessageOutlined className='header-icons'/>
-            <NotificationOutlined className='header-icons' />
-            <Avatar icon={<UserOutlined />} />
+        <Flex align="center" gap="10px">
+          <MessageOutlined className="header-icons" />
+          <NotificationOutlined className="header-icons" />
+          <Avatar icon={<UserOutlined />} />
         </Flex>
       </Flex>
     </Flex>
-  )
-}
+  );
+};
 
-export default HeaderElement
+export default HeaderElement;
